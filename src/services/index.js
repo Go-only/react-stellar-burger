ee32f8618cae569c3415df736f276d8a9d837584
go <macos@MacBook-Pro-Mak.local>
2024-02-24 06:18:21 -0800

    Updates

--- src/services/index.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { rootReducer } from './rootReducers';
-
-export default configureStore({
-	reducer: rootReducer,
-	devTools: process.env.NODE_ENV !== 'production',
-});
