3f1b7fa730423d810d3ee31ebfbe112f070e43ce
go <macos@MacBook-Pro-Mak.local>
2024-02-25 14:20:40 -0800

    fixed type

--- src/components/burger-constructor-item/burger-constructor-item.jsx
+++ src/components/burger-constructor-item/burger-constructor-item.jsx
@@ -1,88 +1,97 @@
-import { ConstructorElement, DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
-import { ingredientSort } from '../../services/slices/burgerConstructorSlice';
+import {
+  ConstructorElement,
+  DragIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import { ingredientSort } from "../../services/slices/burgerConstructorSlice";
 import { useRef } from "react";
 import { useDispatch } from "react-redux";
 import { useDrag, useDrop } from "react-dnd";
 import PropTypes from "prop-types";
 import { ingredientType } from "../../utils/prop-types";
-import styles from './burger-constructor-item.module.css';
-  
-  function BurgerConstructorItem({ item, index, handleDeleteIngredient }) {
-    const ref = useRef(null);
-    const dispatch = useDispatch();
-  console.log(item);
-    const [{ isDragging }, drag] = useDrag({
-      type: "sortItem",
-      item: () => {
-        return { id: item.id, index };
-      },
-      collect: (monitor) => ({
-        isDragging: monitor.isDragging(),
-      }),
-    });
-  
-    const [{ handlerId }, drop] = useDrop({
-      accept: "sortItem",
-      collect(monitor) {
-        return {
-          handlerId: monitor.getHandlerId(),
-        };
-      },
-      hover(item, monitor) {
-        if (!ref.current) {
-          return;
-        }
-        const dragIndex = item.index;
-        const hoverIndex = index;
-  
-        // Don't replace items with themselves
-        if (dragIndex === hoverIndex) {
-          return;
-        }
-  
-        // Determine rectangle on screen
-        const hoverBoundingRect = ref.current?.getBoundingClientRect();
-  
-        // Get vertical middle
-        const hoverMiddleY =
-          (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-  
-        // Determine mouse position
-        const clientOffset = monitor.getClientOffset();
-  
-        // Get pixels to the top
-        const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-  
-        // Only perform the move when the mouse has crossed half of the items height
-        // When dragging downwards, only move when the cursor is below 50%
-        // When dragging upwards, only move when the cursor is above 50%
-  
-        // Dragging downwards
-        if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-          return;
-        }
-  
-        // Dragging upwards
-        if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-          return;
-        }
-  
-        // Time to actually perform the action
-        dispatch(ingredientSort({ to: dragIndex, from: hoverIndex }));
-  
-        // Note: we're mutating the monitor item here!
-        // Generally it's better to avoid mutations,
-        // but it's good here for the sake of performance
-        // to avoid expensive index searches.
-        item.index = hoverIndex;
-      },
-    });
-  
-    const opacity = isDragging ? 0 : 1;
-    drag(drop(ref));
+import styles from "./burger-constructor-item.module.css";
 
-    return (
-      <li key={item.id} className={styles.item} ref={ref} data-handler-id={handlerId} style={{ opacity }}>
+function BurgerConstructorItem({ item, index, handleDeleteIngredient }) {
+  const ref = useRef(null);
+  const dispatch = useDispatch();
+
+  const [{ isDragging }, drag] = useDrag({
+    type: "sortItem",
+    item: () => {
+      return { id: item.id, index };
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  const [{ handlerId }, drop] = useDrop({
+    accept: "sortItem",
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    hover(item, monitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset();
+
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+
+      // Time to actually perform the action
+      dispatch(ingredientSort({ to: dragIndex, from: hoverIndex }));
+
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex;
+    },
+  });
+
+  const opacity = isDragging ? 0 : 1;
+  drag(drop(ref));
+
+  return (
+    <li
+      key={item.id}
+      className={styles.item}
+      ref={ref}
+      data-handler-id={handlerId}
+      style={{ opacity }}
+    >
       <DragIcon type="primary" />
       <ConstructorElement
         text={item.name}
@@ -92,14 +101,13 @@ import styles from './burger-constructor-item.module.css';
         handleClose={() => handleDeleteIngredient(item)}
       />
     </li>
-    );
-  }
-  
-  BurgerConstructorItem.propTypes = {
-    item: PropTypes.shape(ingredientType).isRequired,
-    index: PropTypes.number.isRequired,
-    handleDeleteIngredient: PropTypes.func,
-  };
-  
-  export default BurgerConstructorItem;
-  
+  );
+}
+
+BurgerConstructorItem.propTypes = {
+  item: ingredientType.isRequired,
+  index: PropTypes.number.isRequired,
+  handleDeleteIngredient: PropTypes.func,
+};
+
+export default BurgerConstructorItem;
