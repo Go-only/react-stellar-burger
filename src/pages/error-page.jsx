86f0a6a445b9a2a0bb506087f38f4c602676f964
go <macos@MacBook-Pro-Mak.local>
2024-03-07 18:29:45 +0300

    added style error

--- src/pages/error-page.jsx
+++ src/pages/error-page.jsx
@@ -4,8 +4,10 @@ import styles from "./error-page.module.css";
 function ErrorPage() {
   return (
     <div className={styles.wrapper}>
-      <h1>404</h1>
-      <p>Этой страницы не существует</p>
+      <h1 className="text text_type_digits-large pb-10">404</h1>
+      <p className="text text_type_digits-medium pb-10">
+        Этой страницы не существует
+      </p>
       <Button
         htmlType="button"
         type="primary"
